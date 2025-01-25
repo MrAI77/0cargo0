a5b4b72d2c1e4749c7d7dfc07ef76d8173b06546
MrAI77 <154350833+MrAI77@users.noreply.github.com>
2025-01-25 23:02:14 +0500

    All main alright ok lets'go to WMS man

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -13,33 +13,34 @@ const LanguageSwitcher = () => {
   const { language, setLanguage } = useLanguage();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          size="icon"
-        >
-          <Globe className="h-5 w-5" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="bg-white">
-        <DropdownMenuItem
-          onClick={() => setLanguage('ru')}
-        >
-          Русский
-        </DropdownMenuItem>
-        <DropdownMenuItem
+    <div className="relative group">
+      {/* Кнопка глобуса */}
+      <button className="transition-colors">
+        <Globe className="w-6 h-6" />
+      </button>
+
+      {/* Меню смены языка */}
+      <div className="absolute right-0 mt-2 w-32 bg-white border border-gray-200 rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity">
+        <button
           onClick={() => setLanguage('en')}
+          className="block w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100"
         >
           English
-        </DropdownMenuItem>
-        <DropdownMenuItem
+        </button>
+        <button
+          onClick={() => setLanguage('ru')}
+          className="block w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100"
+        >
+          Русский
+        </button>
+        <button
           onClick={() => setLanguage('uz')}
+          className="block w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100"
         >
           O'zbekcha
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </button>
+      </div>
+    </div>
   );
 };
 
